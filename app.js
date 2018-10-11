d70caccbdd1eb1ef1777ec287163fb9895dae326
zbla92 <34164504+zbla92@users.noreply.github.com>
2018-10-11 15:50:21 +0200

    v. 0..0.2
    Setting up first Event Listeners

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 
 // \ovo jr PRVI MODULE
 var budgetController = (function(){
-    var x = 23;
-    // ovo radi budgetController a dole cemo vidjet zasto tako 
-    var add = function (a){
-        return x + a;
-    }
-    return{
-        publicTest: function(b){
-            return add(b);
-        }
-    }
+ 
+    // somecode
     
 })();
 
@@ -25,15 +17,28 @@ var UIController = (function() {
 
 
 
+var ctrlAddItem = function() {
+      // 1. Get the field input data
+     
+     // 2. Add the Item to the budget controller
+     
+     // 3. Add the item to the UI
+     
+     // 4. Calculate the budget on the UI
+    console.log('pusikurac')
+};
+
 // Treci module/ Jonas je lud ako konta da cu ovo naucit ovako na prvu
 var controller = (function(budgetCtrl, UICtrl){
-    var z = budgetCtrl.publicTest(5);
+   
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
     
-    return {
-        anotherPublic: function(){
-            console.log(z);
-        }
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
     }
+
+    });
     
     
 })(budgetController, UIController);
