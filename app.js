31b7059c81ad0e0dfa47c92a0330b94a021200bf
zbla92 <34164504+zbla92@users.noreply.github.com>
2018-10-12 21:24:11 +0200

    v 0.0.82
    Jedva naso bug ,  napravio semu da pravim Iteme

--- app.js
+++ app.js
@@ -8,23 +8,54 @@ var budgetController = (function(){
         this.value = value;
     };
     
-    var Income = function(){
+    var Income = function(id, description, value){
         this.id = id;
         this.description = description;
         this.value = value;
-    }
+    };
     
     var totalExpenses = 0;
     
     var data = {
         allItems: {
-            exp: [],
-            inc: []
+            exp: [0],
+            inc: [0]
         },
         totals: {
             exp: 0,
-            inx: 0
+            inc: 0
         }
+    };
+    
+    return{
+        addItem: function(type, des, val) {
+            var newItem, ID;
+            
+            //Create New ID
+            if(data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            }else{
+                ID = 0;
+            };
+            
+            //Create new item based on exp or Inc Type
+            if(type === 'exp'){
+                newItem = new Expense(ID, des, val);  
+            }else if(type == 'inc'){
+                newItem = new Income(ID, des, val);
+            }
+            
+            
+            //Return the new element
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+            
+            //testing
+            testing: function(){
+                console.log(data);
+            }
+        
     }
     
 })();
@@ -36,7 +67,7 @@ var budgetController = (function(){
 // UI CONTROLLER   module2
 var UIController = (function() {
     
-    var DOMStrings = {
+    var DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
@@ -45,17 +76,16 @@ var UIController = (function() {
     
     return{
         getInput: function(){
-            return{
-                
-            type: document.querySelector(DOMStrings.inputType).value, // Will be ither Inc or exp  (income or expense)
-            description: document.querySelector(DOMStrings.inputDescription).value,
-            value: document.querySelector(DOMStrings.inputValue).value
-                
+            return{                
+                type: document.querySelector(DOMstrings.inputType).value, // Will be ither Inc or exp  (income or expense)
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+
             }
             
         },
-        getDomStrings: function(){
-            return DOMStrings;
+        getDOMstrings: function(){
+            return DOMstrings;
         }
     }
     
@@ -69,28 +99,28 @@ var UIController = (function() {
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-
+    
     var setupEventListeners = function(){
-    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-    
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
-                ctrlAddItem();
-    }
+        var DOM = UICtrl.getDOMstrings();
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
-    });
-        
-        
- }
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                    ctrlAddItem();
+                }
+            });
+    };
     
-    var DOM = UICtrl.getDomStrings();
-
+    
+        
     var ctrlAddItem = function() {
+        var input, newItem;
          
         // 1. Get the field input data
-            var input = UICtrl.getInput();
+           input = UICtrl.getInput();
 
          // 2. Add the Item to the budget controller
+           newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
          // 3. Add the item to the UI
 
