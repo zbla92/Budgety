2b50c43510ff3853d5420de2c88ab783c17a3773
zbla92 <34164504+zbla92@users.noreply.github.com>
2018-10-15 10:49:38 +0200

    v 0.6.77
    Creating Different Modules:
     - Separation of Concers

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// STARTING OVER
+///////////////////////////////////////////////////////////////
+/////////////// Module 1: BUDGET CONTROLLER ///////////////////
+///////////////////////////////////////////////////////////////
+var budgetController = (function(){
+    
+    var x = 23;
+    
+    var add = function(a){
+        return x + a;
+    }
+    // Ovaj return vraca Motodu praznog objekta i na taj nacin omogucujem komunikaciju sa  ostalim modulima
+    return {
+        publicTest: function(b){
+            
+            console.log(add(b));
+        }
+    }
+})();
+
+
+///////////////////////////////////////////////////////////////
+////////////////Module 2: UI CONTROLLER ///////////////////////
+///////////////////////////////////////////////////////////////
+var UIController = (function(){
+    
+    
+})();
+
+
+
+
+
+///////////////////////////////////////////////////////////////
+////////////Module 3: APP CONTROLLER  /////////////////////////
+///////////////////////////////////////////////////////////////
+var controller = (function(budgetCtrl, UICtrl){
+    // Trazim modulu 2 varijable, koje  predstavljaju dva modula odozgo, jer app controller ce sve kontrolisati
+    
+   var z = budgetCtrl.publicTest(1) // Ovdje sam iskoristio Metodu Budget Controller Modula sto je bas cool.
+   
+   return{
+       bacajVani: function(){
+           console.log(z);
+       }
+   }
+   
+})(budgetController, UIController);
+
+controller.bacajVani();
