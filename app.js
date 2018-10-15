d6fc03101cb4bea2f4bb55f48bb6f8bc5e28222c
zbla92 <34164504+zbla92@users.noreply.github.com>
2018-10-15 11:31:28 +0200

    v 0.6.78
    * How to set up event listeners for keypress events
    * How to use event objects

--- app.js
+++ app.js
@@ -3,18 +3,7 @@
 ///////////////////////////////////////////////////////////////
 var budgetController = (function(){
     
-    var x = 23;
-    
-    var add = function(a){
-        return x + a;
-    }
-    // Ovaj return vraca Motodu praznog objekta i na taj nacin omogucujem komunikaciju sa  ostalim modulima
-    return {
-        publicTest: function(b){
-            
-            console.log(add(b));
-        }
-    }
+
 })();
 
 
@@ -31,19 +20,47 @@ var UIController = (function(){
 
 
 ///////////////////////////////////////////////////////////////
-////////////Module 3: APP CONTROLLER  /////////////////////////
+////////////Module 3: GLOBAL APP CONTROLLER  //////////////////
 ///////////////////////////////////////////////////////////////
 var controller = (function(budgetCtrl, UICtrl){
     // Trazim modulu 2 varijable, koje  predstavljaju dva modula odozgo, jer app controller ce sve kontrolisati
     
-   var z = budgetCtrl.publicTest(1) // Ovdje sam iskoristio Metodu Budget Controller Modula sto je bas cool.
-   
-   return{
-       bacajVani: function(){
-           console.log(z);
-       }
-   }
-   
+    
+    // Funkcija koju pozivamo NA KLIK ili na ENTER PRESS
+    var ctrlAddItem = function (){
+        
+        // 1. Get the field input data
+        
+        // 2. Add the item to the budget controller
+        
+        // 3. Add the item to the UI
+        
+        // 4. Calculate the budget
+        
+        // 5. Display the budget in the UI
+        console.log('Radi')
+        
+        
+    };
+    
+    
+    
+    
+    // Event Listener,  za CLICK NA zelenu kvacicu BTN
+    document.querySelector('.add__btn').addEventListener('click', function(){
+        //Pozicam funkciju jer sam klikno kvacicu
+        ctrlAddItem();
+        
+        
+    });
+    //Event Listener kad neko stisne enter bilo gdje
+    document.addEventListener('keypress', function(event){
+        if (13 === event.keyCode || event.which === 13){
+            // Pozivam funkciju jer sam stisno enter
+           ctrlAddItem();
+        }
+    })
+    
+ 
 })(budgetController, UIController);
 
-controller.bacajVani();
