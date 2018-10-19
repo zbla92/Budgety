8b3e950feb633338b8060a8bcf9edf8b1c562e41
zbla92 <34164504+zbla92@users.noreply.github.com>
2018-10-19 23:07:40 +0200

    v 0.6.90 not finished
    HOW TO USE EVENT DELEGATION
    
    How to use iDs in HTML to connect the UI with the data model
    
    How to use the parrentNode propery for DOM traversing

--- app.js
+++ app.js
@@ -125,7 +125,8 @@ var UIController = (function(){
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
         expenseLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage'
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container'
     };
     
     return {
@@ -146,11 +147,11 @@ var UIController = (function(){
             
             if (type === 'inc'){
                 element = DOMstrings.incomeContainer;
-                 html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'
+                 html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'
             }else if(type === 'exp'){
                 element = DOMstrings.expensesContainer;
                 
-                  html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>  </div></div> </div>'
+                  html = '<div class="item clearfix" id="exp-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>  </div></div> </div>'
             }
             
             
@@ -241,7 +242,9 @@ var controller = (function(budgetCtrl, UICtrl){
                     // Pozivam funkciju jer sam stisno enter
                    ctrlAddItem();
                 }
-            })
+              });
+            
+            document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem)
     };
     
     var updateBudget = function(){
@@ -281,6 +284,19 @@ var controller = (function(budgetCtrl, UICtrl){
             
         }
     };
+    
+        var ctrlDeleteItem = function(event){
+            var itemID,splitID;
+            
+           itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;  
+            console.log(itemID)
+            
+            if (itemID){
+                
+                //inc-1
+                splitID = itemID.split('-')
+            }
+        };
     
     
     return{
