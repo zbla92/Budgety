c6ce0510a3eaac93e408d506520318dec74be257
zbla92 <34164504+zbla92@users.noreply.github.com>
2018-10-20 08:43:40 +0200

    v 0.6.94
    Reinforcing the concepts and techniques we learned so far

--- app.js
+++ app.js
@@ -250,58 +250,70 @@ var controller = (function(budgetCtrl, UICtrl){
     var setupEventListeners = function(){
         var DOM = UICtrl.getDOMstrings();
         
-                    // Event Listener,  za CLICK NA zelenu kvacicu BTN
-            document.querySelector(DOM.inputBtn).addEventListener('click', function(){
-                //Pozicam funkciju jer sam klikno kvacicu
-                ctrlAddItem();
+            // Event Listener,  za CLICK NA zelenu kvacicu BTN
+                document.querySelector(DOM.inputBtn).addEventListener('click', function(){
+                    //Pozicam funkciju jer sam klikno kvacicu
+                    ctrlAddItem();
 
 
-            });
+                });
             //Event Listener kad neko stisne enter bilo gdje
-            document.addEventListener('keypress', function(event){
-                if (13 === event.keyCode || event.which === 13){
-                    // Pozivam funkciju jer sam stisno enter
-                   ctrlAddItem();
-                }
-              });
-            
-            document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem)
+                document.addEventListener('keypress', function(event){
+                    if (13 === event.keyCode || event.which === 13){
+                        // Pozivam funkciju jer sam stisno enter
+                       ctrlAddItem();
+                    }
+                  });
+
+                document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem)
     };
     
     var updateBudget = function(){
         
         // 1. Calculate the  budget
-        budgetCtrl.calculateBudget();
+            budgetCtrl.calculateBudget();
         
         // 2. Return budget 
-        var budget = budgetCtrl.getBudget();
+            var budget = budgetCtrl.getBudget();
         
         // 3. Display the budget on the UI
-        UICtrl.displayBudget(budget);
-    }
-    
+            UICtrl.displayBudget(budget);
+    },
+
+    var updatePercentages = function(){
+
+            // 1. Calculate percentages
+
+            // 2. Read percentages from the budget controller
+
+            // 3. Update the UI with the new percanteges
+    };
+
     
     // Funkcija koju pozivamo NA KLIK ili na ENTER PRESS
     var ctrlAddItem = function (){
         var input, newItem;
         
-        // 1. Get the field input data
-        input = UICtrl.getInput();
-       
-        if (input.description !== "" && !isNaN(input.value) && input.value > 0){
+            // 1. Get the field input data
+                input = UICtrl.getInput();
+
+                if (input.description !== "" && !isNaN(input.value) && input.value > 0){
 
             // 2. Add the item to the budget controller
-            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+                newItem = budgetCtrl.addItem(input.type, input.description, input.value);
             
             // 3. Add the item to the UI
-            UICtrl.addListItem(newItem, input.type);
+                UICtrl.addListItem(newItem, input.type);
             
 
             // 4. Clear the fields;
-            UICtrl.clearFields();
+                UICtrl.clearFields();
 
             // 5. Display the budget in the UI
-           updateBudget();
+                updateBudget();
+            
+            // 6. Calculate and Update percentages
+                updatePercentages();
             
         }
     };
@@ -314,18 +326,22 @@ var controller = (function(budgetCtrl, UICtrl){
             
             if (itemID){
                 
-                //inc-1
+                //inc-1 je input   array ['inc', 1] je output
                 splitID = itemID.split('-');
                 type = splitID[0];
                 ID = parseInt(splitID[1]);
                 
                 //1. Delete the item from the data structure
                     budgetCtrl.deleteItem(type, ID);
+                
                 //2. Delete the item from the UI
                     UICtrl.deleteListItems(itemID);
                 
                 //3. Update ad show the new budget
                     updateBudget();
+                
+                // 4. Calculate and update percentages
+                    updatePercentages();
             }
         };
     
